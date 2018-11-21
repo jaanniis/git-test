9176db1ac03ea9b2e87ac141df8b9eedd213ebde
jaanniis <jannis_v@hotmail.de>
2018-11-21 12:56:09 +0100

    Initial Commit

---
+++ git-test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  git-test
+//
+//  Created by Jannis on 21.11.18.
+//  Copyright © 2018 Jannis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
